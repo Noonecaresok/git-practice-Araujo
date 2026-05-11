c1f3098d3b8b100cf3bcccc1ca1a5e5ddafbb87e
David Araujo <david.araujo.vera@gmail.com>
2026-05-11 15:19:00 -0400

    Update prueba.cpp

--- prueba.cpp
+++ prueba.cpp
@@ -6,5 +6,6 @@ int main (){
     cout<<"Hola mundo";
     
     cout<<"Nuevo codigo agregado, para un siguiente commit, el segundo.";
+    cout<<"Esto esta siendo editado desde github"; //Osi osi
     return 0;
 }
