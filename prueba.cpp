05db796292bad6d6a789a2c55af5c264566bb47b
David Araujo <david.araujo.vera@gmail.com>
2026-05-11 15:24:20 -0400

    Ultimo commit de prueba, antes de pasar a branches, esto desde git

--- prueba.cpp
+++ prueba.cpp
@@ -7,5 +7,7 @@ int main (){
     
     cout<<"Nuevo codigo agregado, para un siguiente commit, el segundo.";
     cout<<"Esto esta siendo editado desde github"; //Osi osi
+
+    cout<<"Un ultimo commit de prueba antes de pasar a branches"; //Desde git
     return 0;
 }
